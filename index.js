1441b273b9c79e3e1d1fd7e2bf06c7b9704e4e39
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2019-08-27 19:03:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function writeCards(names) {
   const result = []
   for (let i = 0; i < names.length; i++) {
-    
+    result.push('Thank you, '+ names[i] +', for the wonderful surprise gift!')
   }
   return result
 }
